d49d9fafa5676676d2f90d0216fe82321de1ab5f
danyel murray <danyel@danyels-MacBook-Pro.local>
2018-03-02 14:51:50 -0800

    add fifth problem to html, begin writing solution in js

---
+++ js/alphabet.js
@@ -0,0 +1,6 @@
+export class Alphabet {
+constructor(){}
+
+
+
+}
--- js/letterChange.js
+++ js/letterChange.js
@@ -6,10 +6,15 @@ export class letterChange {
 letterChange() {
   var letters = string.split(" ");
 
-  for (var i = 0; i < letters.length; i++) {
-    letterss[i] = letters[i].substring(0,1).toUpperCase() + letters[i].substring(1);
-  }
+// start by splitting the word into two parts and then combining them
+// capitalize th first part and combine with the rest of the string
+for (var i = 0; i < letters.length; i++) {
+  letters[i] = letters[i].substring(0,1).toUpperCase() + letters[i].substring(1);
+}
 
-  return letters.join(" ");
+// return the array of letters joined into a string
+return letters.join(" ");
+
+}
 
 }
